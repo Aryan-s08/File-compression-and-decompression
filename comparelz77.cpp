c1e4a9c635388d2c707c2a47ea313eda4d0604da
Aryan <gujarattitans203@gmail.com>
2025-10-24 14:18:48 +0530

    Use for comparing files
    2 arguments which are filenames

---
+++ comparelz77.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <input-file>\n";
+        return 1;
+    }
+    string f1name = argv[1];
+    string f2name = argv[2];
+    ifstream fin(f1name);
+    if (!fin) {
+        cerr << "Error: cannot open input file '" << f1name << '\n';
+        return 2;
+    }
+
+    string f1, f2;
+    int ch;
+    while((ch = fin.get()) != EOF) {
+        f1.push_back(char(ch));
+    }
+    fin.close();
+    fin.open(f2name);
+    if (!fin) {
+        cerr << "Error: cannot open queue file '" << f2name << '\n';
+        return 2;
+    }
+    while((ch = fin.get()) != EOF) {
+        f2.push_back(char(ch));
+    }
+    fin.close();
+    if (f1 != f2) {
+        printf("Files are not the same");
+    } else {
+        printf("Files are the same");
+    }
+}
